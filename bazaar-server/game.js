f17a579cefc143cbd82fb3ae1a81d0d2fd15ade1
Paul Forsans <polfor33@gmail.com>
2022-06-29 15:02:12 +0200

    Gestion des alertes

--- bazaar-server/game.js
+++ bazaar-server/game.js
@@ -1,52 +1,79 @@
+const Game = require("./game");
 const Player = require('./player');
-seedrandom = require('seedrandom');
-const cardsTemplate = {
-    "cards": [{
-        "current_position": "",
-        "new_position": "",
-        "type": "",
-        "value": ""
-    }],
-    "deck": ""
-}
-const cards = require('./cards.json');
-
-class Game {
-
-    room;
-    seed;
-    deck = [];
-    market = [];
-    players = [];
-
-    constructor(roomName, player1, player2) {
-        this.room = roomName;
-        this.seed = seedrandom(roomName)
-        this.players.push(player1, player2);
+const Express = require("express");
+const Http = require('http').Server(Express)
+const SocketIo = require("socket.io")(Http, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
     }
-    createGame() {
-        cards.cards.slice(0, 3).forEach(card => {
-            this.market.push(card)
-        })
-        this.deck = this.shuffleCards(cards.cards.slice(3))
-        this.deck.splice(0, 2).forEach(card => {
-            this.market.push(card)
+})
+var randomWords = require('random-words');
+const { electron } = require("webpack");
+
+var rooms = []
+
+var games = []
+
+SocketIo.on('connection', socket => {
+    socket.on('createroom', data => {
+        let roomname = randomWords({
+            exactly: 3,
+            join: '',
+            formatter: (word) => {
+                return word.slice(0, 1).toUpperCase().concat(word.slice(1))
+            }
         });
-        console.log(this.market)
-        console.log(this.deck.length)
-    }
 
-    shuffleCards(array) {
-        let curId = array.length;
-        while (0 !== curId) {
-            let randId = Math.floor(this.seed.quick() * curId);
-            curId -= 1;
-            let tmp = array[curId];
-            array[curId] = array[randId];
-            array[randId] = tmp;
+        while (rooms[roomname] != undefined) {
+            roomname = randomWords({
+                exactly: 3,
+                join: '',
+                formatter: (word) => {
+                    return word.slice(0, 1).toUpperCase().concat(word.slice(1))
+                }
+            });
         }
-        return array;
-    }
-}
 
-module.exports = Game;
+        rooms[roomname] = { player1: { name: data.name, socket: socket.id }, player2: {} };
+        socket.join(roomname);
+        console.log('le joueur ' + data.name + ' (socket ' + socket.id + ') a rejoit le salon ' + roomname);
+        SocketIo.to(roomname).emit("roomjoined", { room: roomname });
+    });
+
+    socket.on('joinroom', data => {
+        let roomname = data.room
+        let playername = data.name;
+
+        if (rooms[roomname] != undefined) {
+            if (SocketIo.of("/").adapter.rooms.get(roomname).size < 2) {
+                rooms[roomname].player2 = { name: playername, socket: socket.id };
+                socket.join(roomname);
+                console.log('le joueur ' + playername + ' (socket ' + socket.id + ') a rejoint le salon ' + roomname);
+                SocketIo.to(roomname).emit("roomjoined", { room: roomname });
+                if (SocketIo.of("/").adapter.rooms.get(roomname).size == 2) {
+                    startGame(roomname);
+                }
+            } else {
+                socket.emit("alert", { type: "error", message: "Il y a déjà 2 joueurs dans ce salon" })
+            }
+        } else {
+            socket.emit("noroom", { room: roomname })
+            console.log('le joueur ' + playername + ' (socket ' + socket.id + ') a essayé de rejoindre le salon ' + roomname + ' mais il n\'existe pas');
+            socket.emit("alert", { type: "error", message: "Le salon " + roomname + " n'existe pas" })
+
+        }
+    })
+})
+
+
+Http.listen(3000, () => {
+    console.log('listening at 3000');
+})
+
+
+
+function startGame(roomName) {
+    games[roomName] = (new Game(SocketIo, roomName, new Player(rooms[roomName].player1.socket, rooms[roomName].player1.name), new Player(rooms[roomName].player2.socket, rooms[roomName].player2.name)));
+    games[roomName].createGame();
+}
