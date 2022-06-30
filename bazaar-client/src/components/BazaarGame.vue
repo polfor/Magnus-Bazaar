61e276986c33ea6a954bab3edb0af7baee037ba3
Travers Nicolas <nicolas.travers@etu.u-bordeaux.fr>
2022-06-30 16:28:54 +0200

    :sparkles: Add design to board

--- bazaar-client/src/components/BazaarGame.vue
+++ bazaar-client/src/components/BazaarGame.vue
@@ -1,15 +1,17 @@
 <template>
-  <div>
+  <div v-if="lobby == false" class="bazaarGame">
     <!-- Hand player one -->
-    <div class="player-cards player-cards-one">
+    <div @mouseenter="handOpen = true" @mouseleave="handOpen = false" v-bind:class="{ open: handOpen }" class="player-cards player-cards-one">
       <!-- Hand -->
-      <div class="hand">
-        <img v-for="index in 5" :key="index" class="little-card card-player" src="@/assets/Back_card.png" alt="">
+      <div class="hand-container-one">
+        <div class="hand">
+          <img @click="active" v-for="index in 7" :key="index" class="little-card card-player" src="@/assets/Back_card.png" alt="">
+        </div>
       </div>
       
       <!-- Enclosure -->
-      <div class="enclosure">
-        <img v-for="enclosure in playerOne.enclosure" :key="enclosure.index" class="card card-rotate" src="@/assets/Camel_card.png" alt="">
+      <div class="enclosure enclosure-one">
+        <img @click="active" v-for="enclosure in playerOne.enclosure" :key="enclosure.index" class="little-card camel-card camel-card-one" src="@/assets/Camel_card.png" alt="">
       </div>
     </div>
 
@@ -17,14 +19,16 @@
 
     <!-- Hand player two -->
     <div class="player-cards player-cards-two">
-      <!-- Hand -->
-      <div class="hand">
-        <img v-for="index in 5" :key="index" class="little-card card-player" src="@/assets/Back_card.png" alt="">
-      </div>
-      
       <!-- Enclosure -->
-      <div class="enclosure">
-        <img v-for="enclosure in playerTwo.enclosure" :key="enclosure.index" class="card card-rotate" src="@/assets/Camel_card.png" alt="">
+      <div>
+        <img class="little-card" src="@/assets/Camel_card.png" alt="">
+      </div>
+
+      <!-- Hand -->
+      <div class="hand-container-two">
+        <div class="hand">
+          <img v-for="index in 5" :key="index" class="little-card card-player" src="@/assets/Back_card.png" alt="">
+        </div>
       </div>
     </div>
 
@@ -35,13 +39,54 @@
       <div class="board">
         <!-- Tokens -->
         <div class="tokens">
-
+          <!-- Ruby -->
+          <div class="token-list">
+            <img v-for="index in 3" :key="index" class="token" src="@/assets/Ruby_coin-5.png" alt="">
+            <img v-for="index in 2" :key="index" class="token" src="@/assets/Ruby_coin-7.png" alt="">
+          </div>
+          <!-- Gold -->
+          <div class="token-list">
+            <img v-for="index in 3" :key="index" class="token" src="@/assets/Gold_coin-5.png" alt="">
+            <img v-for="index in 2" :key="index" class="token" src="@/assets/Gold_coin-6.png" alt="">
+          </div>
+          <!-- Silver -->
+          <div class="token-list">
+            <img v-for="index in 5" :key="index" class="token" src="@/assets/Silver_coin.png" alt="">
+          </div>
+          <!-- Carpet -->
+          <div class="token-list">
+            <img v-for="index in 2" :key="index" class="token" src="@/assets/Carpet_coin-1.png" alt="">
+            <img v-for="index in 2" :key="index" class="token" src="@/assets/Carpet_coin-2.png" alt="">
+            <img v-for="index in 2" :key="index" class="token" src="@/assets/Carpet_coin-3.png" alt="">
+            <img class="token" src="@/assets/Carpet_coin-5.png" alt="">
+          </div>
+          <!-- Spice -->
+          <div class="token-list">
+            <img v-for="index in 2" :key="index" class="token" src="@/assets/Spice_coin-1.png" alt="">
+            <img v-for="index in 2" :key="index" class="token" src="@/assets/Spice_coin-2.png" alt="">
+            <img v-for="index in 2" :key="index" class="token" src="@/assets/Spice_coin-3.png" alt="">
+            <img class="token" src="@/assets/Spice_coin-5.png" alt="">
+          </div>
+          <!-- Leather -->
+          <div class="token-list">
+            <img v-for="index in 6" :key="index" class="token" src="@/assets/Leather_coin-1.png" alt="">
+            <img class="token" src="@/assets/Leather_coin-2.png" alt="">
+            <img class="token" src="@/assets/Leather_coin-3.png" alt="">
+            <img class="token" src="@/assets/Leather_coin-4.png" alt="">
+          </div>
         </div>
 
         <!-- Market -->
         <div class="market">
-          <img v-for="index in 3" :key="index" class="card" src="@/assets/Camel_card.png" alt="">
+          <img @click="active" v-for="index in 3" :key="index" class="card" src="@/assets/Camel_card.png" alt="">
           <div v-for="index in 2" :key="index" class="card empty-cards"></div>
+
+          <!-- Buttons -->
+          <div class="buttons">
+            <button class="lien">Échanger</button>
+            <button class="lien">Prendre</button>
+            <button class="lien">Vendre</button>
+          </div>
         </div>
 
         <!-- Interface -->
@@ -76,27 +121,45 @@
 
 export default {
   name: 'BazaarGame',
-  props : ['socket'],
+  props : ['socket', 'lobby'],
   data () {
     return {
+      handOpen: false,
+
       playerOne: {
+        playerNo: "",
         hand: [],
-        playerNo: "efz",
-        enclosure: true
+        enclosure: ['fzv', 'fqer', 'fedrz']
       },
       playerTwo: {
+        playerNo: "",
         hand: [],
-        playerNo: "afergeg",
-        enclosure: false
+        enclosure:  ['fzv', 'fqer']
       },
       board: {
         market: [],
         deck: "full",
         graveyard: []
-      }
+      },
+      tradedCards: [],
+      takenCards: []
     }
   },
   methods: {
+    requeteTest() {
+        this.socket.emit('trade', {
+          // Carte dans la main
+          tradedCards: [],
+
+          // Carte dans le marché
+          takenCards: []
+        })
+    },
+
+    active(ev) {
+      ev.target.classList.toggle("active");
+    },
+
     sell:  () => {
       this.socket.emit('sell');
     },
@@ -105,33 +168,69 @@ export default {
       this.socket.emit('buy')
     },
 
-  }, mounted () {
+  }, 
+  mounted () {
+    this.socket.on('game-start', data=> {
+      this.playerNo = data.playerNo
+    })
 
-      this.socket.on('game-start', data=> {
-        this.playerNo = data.playerNo
-      })
-
-      this.socket.on('game-update', data => {
-        if(this.hand != data[this.playerNo].hand) this.hand = data[this.playerNo].hand;
-        if(this.market != data[this.playerNo].market) this.market = data[this.playerNo].market;
-        if(this.oppHand != data[this.playerNo].oppHand) this.oppHand = data[this.playerNo].oppHand;
-      })
-    }
+    this.socket.on('game-update', data => {
+      if(this.playerOne.hand != data[this.playerOne.playerNo].hand) this.playerOne.hand = data[this.playerOne.playerNo].hand;
+      if(this.board.market != data[this.playerOne.playerNo].market) this.market = data[this.playerOne.playerNo].market;
+      if(this.playerTwo.hand != data[this.playerOne.playerNo].oppHand) this.playerTwo.hand = data[this.playerOne.playerNo].oppHand;
+    })
+  }
 
 }
 </script>
 
 <style scoped>
+.bazaarGame {
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+}
+
+/* Buttons */
+.buttons {
+  display: flex;
+  gap: 1rem;
+  position: absolute;
+  top: 110%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 20;
+}
+
 /* Cards */
 .card, .little-card {
   width: 10vw;
   height: auto;
+  border-radius: .7vw;
+}
+
+.card.active, .little-card.active, .little-card.camel-card.active {
+  outline: 2px solid rgb(var(--secondary-color));
+  outline-offset: 2px;
+}
+
+.open .card.active, .open .little-card.active {
+  position: relative;
+  bottom: 2rem;
+}
+
+.open .little-card.camel-card.active{
+  position: relative;
+  bottom: 0;
+  right: 1rem;
+  transform: rotate(-12deg);
 }
 
 .little-card {
   width: 10vw;
   max-width: 100px;
   height: auto;
+  transition: width .5s ease-in-out;
 }
 
 .empty-cards {
@@ -146,6 +245,27 @@ export default {
 
 .empty-cards>img {
   width: 100%;
+}
+
+/* Tokens */
+.tokens {
+  max-width: 20vw;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding-left: 1rem;
+}
+
+.token-list {
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 17%;
+}
+
+.token {
+  width: 5vw;
+  height: auto;
+  margin-right: -20%;
 }
 
 /* Players */
@@ -179,22 +299,49 @@ export default {
   position: absolute;
 }
 
-.hand {
-  display: flex;
-}
-
-.card-player {
-  margin-right: -12%;
-}
-
 .player-cards-one {
-  right: 0;
+  z-index: 10;
+  right: 8vw;
   bottom: -2rem;
 }
 
 .player-cards-two {
-  left: 10vw;
+  left: 8vw;
   top: -2rem;
+}
+
+/* -- -- Hand */
+.hand {
+  display: flex;
+  margin-right: 6vw;
+}
+
+.hand-container-one {
+  align-self: flex-end;
+}
+
+.hand-container-two {
+  align-self: flex-start;
+}
+
+/* -- -- Enclosure */
+.enclosure {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-width: 10vw;
+  max-width: 100px;
+}
+
+.camel-card {
+  position: absolute;
+  width: 100%;
+  height: auto;
+}
+
+/* -- -- Merchandise cards */
+.card-player {
+  margin-right: -6vw;
 }
 
 /* Game */
@@ -217,6 +364,8 @@ export default {
 
 /* -- Market */
 .market {
+  position: relative;
+  max-width: 65vw;
   grid-column: 1 / span 5;
   display: flex;
   gap: 1rem;
@@ -224,6 +373,7 @@ export default {
 
 /* -- Game cards */
 .interface {
+  max-width: 15vw;
   position: relative;
   display: flex;
   flex-direction: column;
@@ -236,5 +386,48 @@ export default {
   display: grid;
   grid-template-rows: repeat(2, 1fr);
   gap: .5rem;
+}
+
+/* ----------------------- */
+/* ------ Animation ------ */
+/* ----------------------- */
+
+/* Hand */
+.player-cards-one:hover .little-card{
+  width: 14vw;
+  max-width: none;
+}
+
+.player-cards-one:hover .enclosure{
+  margin-top: 18vw;
+}
+
+.player-cards-one:hover .camel-card{
+  margin-top: -26vh;
+  position: relative;
+}
+
+.player-cards-one .little-card:hover{
+  position: relative;
+  bottom: 2rem;
+  cursor: pointer;
+}
+
+.player-cards-one  .little-card.camel-card:hover{
+  position: relative;
+  bottom: 0;
+  right: 1rem;
+  transform: rotate(-12deg);
+}
+
+.market .card:hover{
+  cursor: pointer;
+}
+
+@media (min-width: 1280px) { 
+  .player-cards-one:hover .little-card{
+    width: 11vw;
+    max-width: none;
+  }
 }
 </style>
