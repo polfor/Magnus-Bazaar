c5ff9a0d8e5d8408a9718a90e8f4739b0caabdce
Arthur Quemeras <arthur.quemeras@gmail.com>
2022-07-04 11:26:23 +0200

    Fin Pop up écran d'accueil  + début règles du jeu

--- bazaar-client/src/components/LobbySelection.vue
+++ bazaar-client/src/components/LobbySelection.vue
@@ -8,10 +8,12 @@
           Bazaar
         </h1>
         <div class="bouton jeu">
-            <a class="lien">Jouer contre une IA</a>
+          <a class="lien">Jouer contre une IA</a>
         </div>
         <div class="bouton creer">
-          <a @click="isShow_createRoom = !isShow_createRoom" class="lien">Créer un salon</a>
+          <a @click="isShow_createRoom = !isShow_createRoom" class="lien"
+            >Créer un salon</a
+          >
         </div>
 
         <div class="bouton rejoindre">
@@ -33,22 +35,39 @@
       </div>
       <div class="colonne2">
         <img class="image_accueil" src="../assets/marchand.png" alt="" />
-        <svg class="divide_home" width="8" height="100%" viewBox="0 0 8 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0.499955 1024.5L0.499955 -0.5L7.5 -0.5L7.50002 1024.5L0.499955 1024.5Z" fill="#1F1A17" stroke="black"/>
+        <svg
+          class="divide_home"
+          width="8"
+          height="100%"
+          viewBox="0 0 8 1024"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0.499955 1024.5L0.499955 -0.5L7.5 -0.5L7.50002 1024.5L0.499955 1024.5Z"
+            fill="#1F1A17"
+            stroke="black"
+          />
         </svg>
       </div>
     </div>
 
-    <!-- Popup salon -->
+    <!-- Popup salon rejoindre une partie -->
     <div class="pop_up" v-show="isShow_joinRoom">
-      <div class="container">
+      <div class="container_join">
         <h2>Rejoindre un salon</h2>
-        <form id="joinRoom" action="">
-          <input type="text" placeholder="Votre pseudo" />
-          <input type="text" placeholder="Nom du salon" />
-          <input type="submit" class="lien_popup" value="Rejoindre" />
+        <form id="joinRoom" class="join_room" action="">
+          <div class="item_popup">
+            <input type="text" class="input" placeholder="Votre pseudo" />
+          </div>
+          <div class="item_popup">
+            <input type="text" class="input" placeholder="Nom du salon" />
+          </div>
+          <div class="item_popup">
+            <input type="submit" class="lien_popup" value="Rejoindre" />
+          </div>
         </form>
-        <button class="cross" @click="isShow_joinRoom = false">
+        <button class="button_cross" @click="isShow_joinRoom = false">
           <svg
             width="24"
             height="24"
@@ -58,7 +77,7 @@
           >
             <path
               d="M20 20L4 4M20 4L4 20"
-              stroke="#272626"
+              stroke="white"
               stroke-width="2"
               stroke-linecap="round"
             />
@@ -67,35 +86,41 @@
       </div>
     </div>
 
+    <!-- Popup salon créer une partie -->
     <div class="pop_up" v-show="isShow_createRoom">
-      <div class="container">
-        <form id="createRoom" action="">
+      <div class="container_create">
+        <h2>Créer un salon</h2>
+        <form id="createRoom" class="create_room" action="">
           <div>
-            <input type="text" placeholder="Votre pseudo" />
-            <input
-              @click="isShow_createRoom = !isShow_createRoom"
-              class="lien_popup"
-              type="submit"
-              value="Créer"
-            />
+            <div class="item_popup">
+              <input type="text" class="input" placeholder="Votre pseudo" />
+            </div>
+            <div class="item_popup">
+              <input
+                @click="isShow_createRoom = !isShow_createRoom"
+                class="lien_popup"
+                type="submit"
+                value="Créer"
+              />
+            </div>
           </div>
         </form>
-        <button class="cross" @click="isShow_createRoom = false">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M20 20L4 4M20 4L4 20"
-                  stroke="#272626"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                />
-              </svg>
-            </button>
+        <button class="button_cross" @click="isShow_createRoom = false">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M20 20L4 4M20 4L4 20"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   </div>
@@ -103,40 +128,43 @@
 
 <script>
 export default {
-    name: "LobbySelection",
-    data: function () {
-      return {
-        isShow_joinRoom: false,
-        isShow_createRoom: false,
-      };
-    },
-    props: ['socket', 'lobby'],
-    mounted () {
-        const roomCreation = document.querySelector('#createRoom')
-        const roomJoin = document.querySelector('#joinRoom')
-        if (roomCreation) {
-            roomCreation.addEventListener('submit', e => {
-                e.preventDefault()
-                this.socket.emit("createroom", {name : e.target[0].value});
-            })
-        }
-        roomJoin.addEventListener('submit', e => {
-            e.preventDefault()
-            this.socket.emit('joinroom', {name: e.target[0].value, room: e.target[1].value});
-        })
-        this.socket.on('roomjoined', data => {
-            this.emitter.emit('addAlert', {
-                type: "enter",
-                message: data.player + " a rejoint la salle " + data.room,
-            });
-            this.emitter.emit('setRoom', data.room);
-            this.emitter.emit('setLobby', false);
-        })
-        this.socket.on('noroom', data => {
-            console.log('La room '+ data.room + ' n\'existe pas');
-        })
-    },
-}
+  name: "LobbySelection",
+  data: function () {
+    return {
+      isShow_joinRoom: false,
+      isShow_createRoom: false,
+    };
+  },
+  props: ["socket", "lobby"],
+  mounted() {
+    const roomCreation = document.querySelector("#createRoom");
+    const roomJoin = document.querySelector("#joinRoom");
+    if (roomCreation) {
+      roomCreation.addEventListener("submit", (e) => {
+        e.preventDefault();
+        this.socket.emit("createroom", { name: e.target[0].value });
+      });
+    }
+    roomJoin.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.socket.emit("joinroom", {
+        name: e.target[0].value,
+        room: e.target[1].value,
+      });
+    });
+    this.socket.on("roomjoined", (data) => {
+      this.emitter.emit("addAlert", {
+        type: "enter",
+        message: data.player + " a rejoint la salle " + data.room,
+      });
+      this.emitter.emit("setRoom", data.room);
+      this.emitter.emit("setLobby", false);
+    });
+    this.socket.on("noroom", (data) => {
+      console.log("La room " + data.room + " n'existe pas");
+    });
+  },
+};
 </script>
 
 <style scoped>
@@ -174,13 +202,13 @@ export default {
 .lien_popup {
   width: 10rem;
   display: inline-block;
-  color: white;
-  background-color: #272626;
+  color: var(--main-color);
+  background-color: #ddd;
   text-decoration: none;
   font-size: large;
-  padding: 1rem;
+  padding: 0.5rem;
   border-radius: 0.5rem;
-  border: solid 2px #e2c372;
+  border: none;
 }
 .lien_popup:hover {
   background-color: #e2c372;
@@ -205,21 +233,54 @@ export default {
   justify-content: center;
   background: rgba(0, 0, 0, 0.4);
 }
-.container {
+.container_create {
+  align-content: center;
   background-color: var(--main-color);
   color: white;
   border-radius: 1rem;
-  height: 9rem;
+  height: 11rem;
+  width: 22rem;
+  position: relative;
 }
-.cross {
+.container_join {
+  align-content: center;
+  background-color: var(--main-color);
+  color: white;
   border-radius: 1rem;
+  height: 14rem;
+  width: 25rem;
+  position: relative;
+}
+.button_cross {
+  background: none;
   color: inherit;
   border: none;
   padding: 0;
   font: inherit;
   cursor: pointer;
   outline: inherit;
-  position: fixed;
-  top: 41%;
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+}
+
+.join_room {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.create_room {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.item_popup {
+  margin-bottom: 1rem;
+}
+
+.input {
+  border-radius: 0.5rem;
+  height: 1.5rem;
+  text-align: center;
 }
 </style>
