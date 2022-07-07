176af8ae27b1fd16df4e39b527cddea5547dfb95
Paul Forsans <polfor33@gmail.com>
2022-07-07 16:30:23 +0200

    IA nulle prête

--- bazaar-server/ia-game.js
+++ bazaar-server/ia-game.js
@@ -12,32 +12,63 @@ class IAGame extends Game {
     constructor(io, player1) {
         player1.socket.emit('ia-start');
         super(io, player1.socket.id, player1, new IAPlayer())
+        setTimeout(() => {
+            if (this.currentPlayer == 1) {
+                this.iaPlay()
+                this.updateGame();
+            }
+        }, 2000)
     }
 
     buildEvents() {
         this.players[0].socket.on('trade', data => {
-            this.trade(player, data)
-            this.iaPlay();
+            this.trade(this.players[0], data)
             this.updateGame();
+            setTimeout(() => {
+                if (!this.isEnded()) {
+                    this.iaPlay()
+                    this.updateGame();
+                }
+            }, 2000)
         })
 
         this.players[0].socket.on('sell', data => {
-            this.sell(player, data);
-            this.iaPlay();
+            this.sell(this.players[0], data);
             this.updateGame();
+            setTimeout(() => {
+                if (!this.isEnded()) {
+                    this.iaPlay()
+                    this.updateGame();
+                }
+            }, 2000)
         })
     }
 
     iaPlay() {
         /*
-        Choix possibles :
-        *   Vendre toutes les cartes d'un type
-        *   Prendre tous les chameaux du marché
-        *   Prendre une carte non chameau au hasard
-        *   Echanger 
-        *
-        */
+         * Choix possibles :
+         * 
+         *   Vendre toutes les cartes d'un type
+         *   Prendre tous les chameaux du marché
+         *   Prendre une carte non chameau au hasard
+         *   
+         */
+        if (this.players[1].getHand().length >= 7) {
+            this.sell(this.players[1], this.players[1].sellRandom())
+        } else {
+            if (!this.players[1].getHand().length || this.players[1].getIllegalToSell()) {
+                this.trade(this.players[1], this.players[1].pickRandom(this.market));
+            } else {
+                if (Math.random() > .5) {
+                    this.sell(this.players[1], this.players[1].sellRandom());
+                } else {
+                    this.trade(this.players[1], this.players[1].pickRandom(this.market));
+                }
+            }
+        }
+
     }
+
 
 
 }
